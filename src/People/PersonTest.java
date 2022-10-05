43144e779b7bb1498899e2c8f6c490ae48435c8c
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2022-10-05 16:21:09 +0200

    folytatas

--- src/People/PersonTest.java
+++ src/People/PersonTest.java
@@ -2,7 +2,7 @@ package People;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person p1;
+        Person p1 = new Person();
         System.out.println(p1);
     }
 }
