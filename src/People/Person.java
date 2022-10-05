9c214d3d8e278d45bfa062c31474a613e28c8090
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2022-10-05 22:08:55 +0200

    valami

--- src/People/Person.java
+++ src/People/Person.java
@@ -1,8 +1,43 @@
 package People;
 
+import java.util.Objects;
+
 public class Person {
     String id;
     String name;
     int age;
     boolean male;
+
+    public Person(String id, String name, int age, boolean male) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.male = male;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", male=" + male +
+                '}';
+    }
+    public Person(String id, String name, int age) {
+        this(id, name, age, true);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/People/PersonTest.java
+++ src/People/PersonTest.java
@@ -2,7 +2,22 @@ package People;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person p1 = new Person();
+        Person p1 = new Person("A1", "Aladár", 18, true);
         System.out.println(p1);
+        Person p2 = new Person("A1", "Aladár", 18, true);
+        System.out.println(p2);
+        p1.id = "P01";
+        p1.name = "Aladár";
+        p1.age = 18;
+        p1.male = true;
+        System.out.println(p1);
+        System.out.println(p1.id + ' ' + p1.name);
+        System.out.println(p1.toString());
+        Person p3 = p2;
+
+        System.out.println(p1 == p2);
+        System.out.println(p1 == p3);
+        System.out.println(p2 == p3);
+        System.out.println(p1.equals(p2));
     }
 }
